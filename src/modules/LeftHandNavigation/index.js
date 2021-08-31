2374bda68c7085cd59a0b275047fa7edf22f2e50
nauthiz-jera <nauthiz.jera@gmail.com>
2021-08-30 22:59:01 -0400

    fix navigation

--- src/modules/LeftHandNavigation/index.js
+++ src/modules/LeftHandNavigation/index.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { navigateToUrl } from "single-spa";
 
 import { NAVIGATION_ITEMS } from "./../../constants/navigation-items";
 import Navigation from "./styles";
 
 const LeftHandNavigation = () => {
+  const singleSpaNavigation = (path) => (e) => {
+    e.preventDefault();
+    navigateToUrl(path);
+    return false;
+  };
   return (
     <Navigation>
       {NAVIGATION_ITEMS.map(({ name, path }) => (
-        <Navigation.Item key={name} href={path}>
+        <Navigation.Item
+          key={name}
+          onClick={singleSpaNavigation(path)}
+          href={path}
+        >
           {name}
         </Navigation.Item>
       ))}
